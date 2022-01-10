1ce3dbebfb49c767b60968c166ba846e141ed2c2
kieska6 <karl_kieran@yahoo.fr>
2022-01-10 13:28:45 -0500

    Initial Commit

---
+++ Rocket Elevators/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rocket Elevators
+//
+//  Created by Kieran  Kenga  on 2022-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rocket Elevators/Rocket_ElevatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rocket_ElevatorsApp.swift
+//  Rocket Elevators
+//
+//  Created by Kieran  Kenga  on 2022-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Rocket_ElevatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
